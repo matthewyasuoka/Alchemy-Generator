b8a780a3ea5491feaf4c03e403fb225eeb74ce26
matthewyasuoka <mattyaneo@gmail.com>
2022-01-11 15:04:04 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+const link = "https://api.scryfall.com/cards/random?format=image&version=normal&q=mv%3d3t%3Acreature"
+var mv_enter = document.createElement("Input");
+x.setAttribute("type", "text");
