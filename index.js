b7c7f3a95f85974c8071fa008042296812e42324
matthewyasuoka <mattyaneo@gmail.com>
2022-01-11 15:56:45 -0600

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-const link = "https://api.scryfall.com/cards/random?format=image&version=normal&q=mv%3d3t%3Acreature"
-var mv_enter = document.createElement("Input");
-x.setAttribute("type", "text");
